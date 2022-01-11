edcefad63229ff45cae97c3caba935ef1308badf
GrigoriyUstymenko <gromgoriy@gmail.com>
2022-01-11 16:55:02 +0200

    WTF am i doing

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const items = require('./items.json');
 
-app.post('/api/insertItem', (req, res) => {
+app.get('/api/insertItem', (req, res) => {
   res.status(200);
   res.json(items);
 });
